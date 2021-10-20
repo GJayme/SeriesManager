0012be07edd9e3e3640ad3fbe6e2dfecfd545fd1
Gabriel Jayme <gabrielj_89@hotmail.com>
2021-10-20 20:08:15 -0300

    Initial Commit

---
+++ app/src/main/java/sc3005054/jayme/seriesmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package sc3005054.jayme.seriesmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
