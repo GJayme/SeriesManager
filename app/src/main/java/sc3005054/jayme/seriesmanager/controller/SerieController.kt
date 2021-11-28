aada6c2b372f2301caac4d1cbba2e2d73118a56a
Gabriel Jayme <gabrielj_89@hotmail.com>
2021-11-28 18:58:11 -0300

    create SerieFirebase and update SerieController to using firebase rtdb.

--- app/src/main/java/sc3005054/jayme/seriesmanager/controller/SerieController.kt
+++ app/src/main/java/sc3005054/jayme/seriesmanager/controller/SerieController.kt
@@ -3,11 +3,11 @@ package sc3005054.jayme.seriesmanager.controller
 import sc3005054.jayme.seriesmanager.MainSerieActivity
 import sc3005054.jayme.seriesmanager.domain.Serie
 import sc3005054.jayme.seriesmanager.repository.SerieDAO
+import sc3005054.jayme.seriesmanager.repository.SerieFirebase
 import sc3005054.jayme.seriesmanager.repository.SerieSQLite
 
 class SerieController(mainSerieActivity: MainSerieActivity) {
-
-    private val serieDAO: SerieDAO = SerieSQLite(mainSerieActivity)
+    private val serieDAO: SerieDAO = SerieFirebase()
 
     fun inserirSerie(serie: Serie) = serieDAO.criarSerie(serie)
     fun buscarSeries() = serieDAO.recuperarSeries()
---
+++ app/src/main/java/sc3005054/jayme/seriesmanager/repository/SerieFirebase.kt
@@ -0,0 +1,68 @@
+package sc3005054.jayme.seriesmanager.repository
+
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import sc3005054.jayme.seriesmanager.domain.Serie
+
+class SerieFirebase: SerieDAO {
+    companion object {
+        private val BD_SERIES_MANAGER = "series-managager"
+    }
+    // Referência para o RtDb -> series-manager
+    private val  seriesManagerRtDb = Firebase.database.getReference(BD_SERIES_MANAGER)
+
+    // Lista de series que simula uma consulta
+    private val seriesList: MutableList<Serie> = mutableListOf()
+    init {
+        seriesManagerRtDb.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val novaSerie: Serie? = snapshot.value as? Serie
+                novaSerie?.apply {
+                    if (seriesList.find { it.nome == this.nome } == null) {
+                        seriesList.add(this)
+                    }
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val serieEditada: Serie? = snapshot.value as? Serie
+                serieEditada?.apply {
+                    seriesList[seriesList.indexOfFirst { it.nome == this.nome }] = this
+                }
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                val serieRemovida: Serie? = snapshot.value as? Serie
+                serieRemovida?.apply {
+                    seriesList.remove(this)
+                }
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                // Não se aplica
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Não se aplica
+            }
+        })
+    }
+    override fun criarSerie(serie: Serie): Long {
+        criarOuAtualizarSerie(serie)
+        return 0L
+    }
+
+    override fun recuperarSeries(): MutableList<Serie> = seriesList
+
+    override fun removerSerie(nome: String): Int {
+        seriesManagerRtDb.child(nome).removeValue()
+        return 1
+    }
+
+    private fun criarOuAtualizarSerie(serie: Serie) {
+        seriesManagerRtDb.child(serie.nome).setValue(serie)
+    }
+}
